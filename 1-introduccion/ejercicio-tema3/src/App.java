22f4d29c94200723fd568dbf3ca413b9722f54cc
gacevedob <gacevedobastias@gmail.com>
2022-06-21 20:39:59 -0400

    first commit

---
+++ 1-introduccion/ejercicio-tema3/src/App.java
@@ -0,0 +1,28 @@
+/*  */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        suma(1,2,3);
+
+        Coche miCoche = new Coche();
+
+        miCoche.sumarPuertas();
+        miCoche.sumarPuertas();
+        System.out.println("El numero de puertas es: " + miCoche.numeroDePuertas);
+    }
+
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a+b+c;
+        System.out.println(resultado);
+    }
+}
+
+class Coche {
+    public int numeroDePuertas = 2;
+
+    public void sumarPuertas(){
+
+        this.numeroDePuertas++;
+    }
+}
